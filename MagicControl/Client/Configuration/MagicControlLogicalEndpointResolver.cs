8886f8177e1ed0f06d8e52202d7e054c27182217
Magic <131926685+magiccodingman@users.noreply.github.com>
2026-07-20 14:13:52 -0400

    Connect MagicSettings through MagicControl logical endpoints

---
+++ MagicControl/Client/Configuration/MagicControlLogicalEndpointResolver.cs
@@ -0,0 +1,21 @@
+using System.Text.Json.Nodes;
+using MagicControl.Shared.Mesh;
+using MagicSettings;
+using MagicSettings.Share;
+
+namespace MagicControl.Client;
+
+public sealed class MagicControlLogicalEndpointResolver(MagicControlClientOptions options)
+    : IMagicControlPlaneEndpointResolver
+{
+    public MagicResolvedControlPlaneEndpoint Resolve<TSettings>(
+        MagicSettingsOptions<TSettings> settings,
+        JsonObject persistentDocument,
+        Uri? runtimeOverride = null)
+        where TSettings : class, new()
+        => new(
+            runtimeOverride ?? MagicControlLogicalUris.ControlPlaneBase(options.GroupId),
+            runtimeOverride is null
+                ? MagicControlPlaneEndpointSource.CodeFallback
+                : MagicControlPlaneEndpointSource.RuntimeOverride);
+}
