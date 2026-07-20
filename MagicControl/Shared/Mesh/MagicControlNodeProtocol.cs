a2ea4626135438b668b7e09772dfcc3935fd22bd
Magic <131926685+magiccodingman@users.noreply.github.com>
2026-07-20 15:50:51 -0400

    Add peer discovery protocol constants

--- MagicControl/Shared/Mesh/MagicControlNodeProtocol.cs
+++ MagicControl/Shared/Mesh/MagicControlNodeProtocol.cs
@@ -4,7 +4,13 @@ public static class MagicControlNodeProtocol
 {
     public const string NodeSyncAudience = "MagicControl.Node.Sync";
     public const string NodeSecretAudience = "MagicControl.Node.Secret";
+
     public const string DiscoveryMulticastAddress = "239.255.77.77";
     public const int DiscoveryPort = 45873;
     public const int DiscoveryProtocolVersion = 1;
+
+    public const string PeerDiscoveryAudience = "MagicControl.Peer.Discovery";
+    public const string PeerDiscoveryMulticastAddress = "239.255.77.78";
+    public const int PeerDiscoveryPort = 45874;
+    public const int PeerDiscoveryProtocolVersion = 1;
 }
