9e3e082023ce056f59b6f3a9f8e5569cb42e6a15
Magic <131926685+magiccodingman@users.noreply.github.com>
2026-07-20 14:12:55 -0400

    Define MagicControl discovery protocol

---
+++ MagicControl/Shared/Mesh/MagicControlNodeProtocol.cs
@@ -0,0 +1,10 @@
+namespace MagicControl.Shared.Mesh;
+
+public static class MagicControlNodeProtocol
+{
+    public const string NodeSyncAudience = "MagicControl.Node.Sync";
+    public const string NodeSecretAudience = "MagicControl.Node.Secret";
+    public const string DiscoveryMulticastAddress = "239.255.77.77";
+    public const int DiscoveryPort = 45873;
+    public const int DiscoveryProtocolVersion = 1;
+}
